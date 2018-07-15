7d9124e58b8e5646cacbe806fd230eb0bf2d5585
Tomasz Lis <tomasz.lis@o2.pl>
2018-07-15 18:13:38 +0200

    Add basic code from excercises 9.1

---
+++ js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var data = [
+	{
+		id: 'box1',
+		title: 'First box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'special-header', 'important']
+	},
+	{
+		id: 'box2',
+		title: 'Second box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['special-header', 'important']
+	},
+	{
+		id: 'box3',
+		title: 'Third box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'important']
+	},
+	{
+		id: 'box4',
+		title: 'Fourth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted']
+	},
+	{
+		id: 'box5',
+		title: 'Fifth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: []
+	},
+];
